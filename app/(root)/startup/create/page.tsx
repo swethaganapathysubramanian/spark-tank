3fafa75569a9fa25ce556710587ff7ecef99ab8c
swethaganapathysubramanian <swethaganapathy2@gmail.com>
2025-03-04 15:16:22 -0800

    Fixed linting errors and updated config

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -3,19 +3,19 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  const session = await auth();
+    const session = await auth();
 
-  if (!session) redirect("/");
+    if (!session) redirect("/");
 
-  return (
-    <>
-      <section className="pink_container !min-h-[230px]">
-        <h1 className="heading">Submit Your Startup</h1>
-      </section>
+    return (
+        <>
+            <section className="pink_container !min-h-[230px]">
+                <h1 className="heading">Submit Your Startup</h1>
+            </section>
 
-      <StartupForm />
-    </>
-  );
+            <StartupForm />
+        </>
+    );
 };
 
 export default Page;
